9ab51db6c3bc55325353c6b73b7ae1ee8b9de17c
jorge06br <jorgebaca06@gmail.com>
2022-08-11 17:36:13 +0000

    SINGLE CUSTOMERS API,LOGIN PASSWORD ENCRYPTION AND JWT

--- src/api/models.py
+++ src/api/models.py
@@ -9,7 +9,7 @@ class Users(db.Model):
     __tablename__='Users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(50), unique=False, nullable=False)
+    password = db.Column(db.String(500), unique=False, nullable=False)
     name = db.Column(db.String(100), unique=False, nullable=False)
     lastname = db.Column(db.String(100), unique=False, nullable=False)
     lawyer_identification = db.Column(db.String(100), unique=True, nullable=False)
