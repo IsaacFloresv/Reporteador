8955647010b915e94a10e33c9b4d1a81b3859b68
jorge06br <jorgebaca06@gmail.com>
2022-08-19 19:52:31 +0000

    [NEW CLIENT UI PENDING RESPONSIVE BOTTOM]

--- src/front/js/router.js
+++ src/front/js/router.js
@@ -15,6 +15,7 @@ import Tareas from "./pages/tareas/Tareas.jsx";
 import Casos from "./pages/casos/Casos.jsx";
 import Login from "./pages/login/Login.jsx";
 import Signup from "./pages/SignUp/Signup.jsx";
+import NewClient from "./pages/clientes/NewClient.jsx";
 
 //create your first component
 const Router = () => {
@@ -37,6 +38,7 @@ const Router = () => {
             <Route element={<Clientes />} path="/clientes" />
             <Route element={<Documentos />} path="/documentos" />
             <Route element={<Tareas />} path="/tareas" />
+            <Route element={<NewClient/>} path="/newclient"/>
           </Route>
         </Routes>
       </BrowserRouter>
