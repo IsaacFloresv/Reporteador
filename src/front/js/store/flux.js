a6948263dd2562470832e840dc8071f09c4b5309
CarlosLukass <carlos.lukassf@gmail.com>
2022-08-27 02:58:37 +0000

    backend url added

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,189 +1,177 @@
 import { Alert } from "bootstrap";
 
-const URL="https://3001-carloslukass-dropcases-hh891x5gpd2.ws-us63.gitpod.io"
-const getState = ({
-	getStore,
-	getActions,
-	setStore
-}) => {
-	return {
-		store: {
-			token:"",
-			user:{
-				id:"",
-				name:"",
-				lastname:"",
-				email: "",
-				loggedIn: false,
-		},
-			alert: {
-				type: "",
-				msg: "",
-				show: false
-		},
-			status:"",
-			
-		},
-		actions: {
+const URL = process.env.BACKEND_URL;
 
-			setAlert: payload => {
-				setStore({ alert: payload });
-			},
-			clearAlert: () => {
-				setStore({
-					alert: {
-						type: "",
-						msg: "",
-						show: false
-					}
-				});
-			},
-			putCurrentPage: () => {
-				return "hello";
-			},
-			register: (values) => {
-				const myHeaders = new Headers();
-				myHeaders.append("Content-Type", "application/json");
-				const raw = JSON.stringify(values);
-				const requestOptions = {
-					method: "POST",
-					//cors:"no-cors",
-					headers: myHeaders,
-					body: raw
-				};
-				const store=getStore();
-				return fetch(`${URL}/api/user`, requestOptions)
-				.then((response) => response.json())
-				.then(data=>{
-						console.log("data ", data);
-						setStore({
-							user: {
-								...store.user,
-								...data.user
-							},
-							status:data.status
-						});
-						console.log("store", store),
-						console.log("status",store.status)
-						getActions().setAlert({
-							type: "success",
-							msg: data.msg,
-							show: true
-						});
-						
-						return store;
-					});
-			},
+const getState = ({ getStore, getActions, setStore }) => {
+  return {
+    store: {
+      token: "",
+      user: {
+        id: "",
+        name: "",
+        lastname: "",
+        email: "",
+        loggedIn: false,
+      },
+      alert: {
+        type: "",
+        msg: "",
+        show: false,
+      },
+      status: "",
+    },
+    actions: {
+      setAlert: (payload) => {
+        setStore({ alert: payload });
+      },
+      clearAlert: () => {
+        setStore({
+          alert: {
+            type: "",
+            msg: "",
+            show: false,
+          },
+        });
+      },
+      putCurrentPage: () => {
+        return "hello";
+      },
+      register: (values) => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        const raw = JSON.stringify(values);
+        const requestOptions = {
+          method: "POST",
+          //cors:"no-cors",
+          headers: myHeaders,
+          body: raw,
+        };
+        const store = getStore();
+        return fetch(`${URL}/user`, requestOptions)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("data ", data);
+            setStore({
+              user: {
+                ...store.user,
+                ...data.user,
+              },
+              status: data.status,
+            });
+            console.log("store", store), console.log("status", store.status);
+            getActions().setAlert({
+              type: "success",
+              msg: data.msg,
+              show: true,
+            });
 
-			login:(values) => {
-				const myHeaders = new Headers();
-				myHeaders.append("Content-Type", "application/json");
-				const raw = JSON.stringify(values);
-				const requestOptions = {
-					method: "POST",
-					headers: myHeaders,
-					cors:"no-cors",
-					body: raw
-				};
-				const store=getStore();
-				return fetch(`${URL}/api/login`, requestOptions)
-				.then((response) => response.json())
-				.then(data=>{
-					if (typeof data.user === "undefined") throw new Error(data.msg);
-					setStore({
-						user: {
-							...data.user,
-							loggedIn: true
-						},
-						token:data.token
-					});
-					console.log("store",store)
-					localStorage.setItem(
-						"Dropcase",
-						JSON.stringify({
-							token: data.token,
-							email: data.user.email
-						})
-					);
-				})
-				.catch(error => 
-					getActions().setAlert({
-						type: "danger",
-						msg: error.message,
-						show: true
-				}),
-				console.log("alert",alert.msg)
-				);
-			},
-			forgotPassword: email => {
-				
-				return fetch(`${URL}/api/reset`, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify({
-						email: email
-					})
-				})
-					.then(res => res.json())
-					.then(data => {
-						
-						return data.msg;
-					});
-		},
-		checkToken: () => {
-		let tokenCheck = JSON.parse(localStorage.getItem("Dropcases"));
+            return store;
+          });
+      },
 
-				if (tokenCheck !== null) {
-					// token is present, so do something (set loggedIn, maybe?)
-					// console.log(tokenCheck);
-					return fetch(`${URL}/validate`, {
-						headers: {
-							"Content-Type": "application/json",
-							Authorization: `Bearer ${tokenCheck.token}`
-						}
-					})
-						.then(response => {
-							if (response.status === 401) {
-								throw new Error("Token Expired, please login.");
-							}
+      login: (values) => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        const raw = JSON.stringify(values);
+        const requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          cors: "no-cors",
+          body: raw,
+        };
+        const store = getStore();
+        return fetch(`${URL}/login`, requestOptions)
+          .then((response) => response.json())
+          .then((data) => {
+            if (typeof data.user === "undefined") throw new Error(data.msg);
+            setStore({
+              user: {
+                ...data.user,
+                loggedIn: true,
+              },
+              token: data.token,
+            });
+            console.log("store", store);
+            localStorage.setItem(
+              "Dropcase",
+              JSON.stringify({
+                token: data.token,
+                email: data.user.email,
+              })
+            );
+          })
+          .catch(
+            (error) =>
+              getActions().setAlert({
+                type: "danger",
+                msg: error.message,
+                show: true,
+              }),
+            console.log("alert", alert.msg)
+          );
+      },
+      forgotPassword: (email) => {
+        return fetch(`${URL}/reset`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+          }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            return data.msg;
+          });
+      },
+      checkToken: () => {
+        let tokenCheck = JSON.parse(localStorage.getItem("Dropcases"));
 
-							if (!response.ok) throw new Error(response.status);
-							return response.json();
-						})
-						.then(data => {
-							setStore({
-								user: {
-									...getStore().user,
-									...tokenCheck,
-									...data.user,
-									loggedIn: true
-								}
-							});
-						})
-						.catch(error => console.error(error));
-				}
-			},
+        if (tokenCheck !== null) {
+          // token is present, so do something (set loggedIn, maybe?)
+          // console.log(tokenCheck);
+          return fetch(`${URL}/validate`, {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${tokenCheck.token}`,
+            },
+          })
+            .then((response) => {
+              if (response.status === 401) {
+                throw new Error("Token Expired, please login.");
+              }
 
-			uploadFiles: (files, user) =>{
-				for(let i = 0; i < files.length; i++){
-					const formData = new FormData();
-					formData.append("usuario", user);
-					formData.append("archivo", files[i]);
-					fetch(
-					"https://3001-carloslukass-dropcases-od2ynxzu3gx.ws-us63.gitpod.io/api/upload",
-					{
-						method: "POST",
-						body: formData,
-					}
-				).then((res) => console.log(res));
-			}
-			},
+              if (!response.ok) throw new Error(response.status);
+              return response.json();
+            })
+            .then((data) => {
+              setStore({
+                user: {
+                  ...getStore().user,
+                  ...tokenCheck,
+                  ...data.user,
+                  loggedIn: true,
+                },
+              });
+            })
+            .catch((error) => console.error(error));
+        }
+      },
 
-		}
-
-	}
+      uploadFiles: (files, user) => {
+        for (let i = 0; i < files.length; i++) {
+          const formData = new FormData();
+          formData.append("usuario", user);
+          formData.append("archivo", files[i]);
+          fetch(`${URL}/upload`, {
+            method: "POST",
+            body: formData,
+          }).then((res) => console.log(res));
+        }
+      },
+    },
+  };
 };
 
 export default getState;
