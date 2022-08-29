98c4c317f5eafb286dafc8af3f70f8b6d96d2077
CarlosLukass <carlos.lukassf@gmail.com>
2022-08-29 05:37:28 +0000

    VerificationCode component

---
+++ src/front/js/components/Login/VerificationCode.jsx
@@ -0,0 +1,94 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../../store/appContext.js";
+import logo from "../../../../../public/assets/logo.png";
+import { validateCode } from "../ValidateErrors/Errors.jsx";
+
+const VerificationCode = ({ setStage }) => {
+  const { actions, store } = useContext(Context);
+  const [codes, setCodes] = useState({ code: "" });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = ({ target }) => {
+    setCodes({
+      ...codes,
+      [target.name]: target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(codes.code);
+    const errores = validateCode({ code: codes.code });
+    if (Object.keys(errores).length === 0) {
+      let code = await actions.verificationCode({ code: codes.code });
+      console.log(store.status);
+      if (store.alert.msg == "Wrong Code") {
+        console.log("Codigo de verificación incorrecto");
+      }
+      //if(data.msg =="Set new password")
+      setErrors(errores);
+      setStage("NewPassword");
+    }
+  };
+  return (
+    <form
+      className="col-12 col-xl-7  d-flex align-items-center needs-validation"
+      onSubmit={(e) => handleSubmit(e)}
+    >
+      <div className="container">
+        <div className="w-75 m-auto">
+          <div className="col-6 text-center mx-auto ">
+            <img
+              className="text-center"
+              style={{
+                width: 100 + "%",
+              }}
+              src={logo}
+            />
+          </div>
+          <div className="col-xxl-12 py-2">
+            <label
+              htmlFor="validationCustom01"
+              className="form-label px-0 mb-0"
+            >
+              Codigo de verificación
+            </label>
+            <input
+              name="code"
+              type="text"
+              className="form-control"
+              id="validationCustom01"
+              placeholder=""
+              required
+              onChange={(e) => handleChange(e)}
+            />{" "}
+            {errors.code && (
+              <p>
+                {" "}
+                {errors.code}
+                {console.log(codes)}
+              </p>
+            )}
+            <div className="valid-feedback">Looks good!</div>
+            <div className="d-flex justify-content-between">
+              <div className="col-3">
+                <button
+                  className="btn btn-link pe-0 "
+                  onClick={() => setstage("login")}
+                >
+                  <span className=""></span>
+                  Back to login
+                </button>
+              </div>
+              <div className="col-3 my-2">
+                <button className="btn btn-primary col-12" type="submit">
+                  Nueva Contraseña
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
+export default VerificationCode;
