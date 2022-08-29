fe9cb413c156ff3584cdb9674ffde02bc93863f3
CarlosLukass <carlos.lukassf@gmail.com>
2022-08-29 05:58:39 +0000

    deploy

--- src/front/js/pages/login/Login.jsx
+++ src/front/js/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import login_image from "../../../../../public/assets/login-image.png";
 import SendCode from "../../components/Login/SendCode.jsx";
 import NewPassword from "../../components/Login/NewPassword.jsx";
@@ -6,31 +6,34 @@ import LoginForm from "../../components/Login/LoginForm.jsx";
 import VerificationCode from "../../components/Login/VerificationCode.jsx";
 
 const Login = () => {
+  console.log("login");
 
+  const [stage, setstage] = useState("login");
 
-    const [stage, setstage] = useState('login');
-
-    return (
-        <div className="container-fluid fw-bold">
-            <div className="row">
-                <div className="col-xl-5 p-0 d-none d-lg-block vh-100">
-                    <img className="img-responsive"
-                        style={
-                            {objectFit: "cover"}
-                        }
-                        src={login_image}/>
-                </div>
-                {
-                console.log(stage)
-            }
-                {
-                stage === 'login' ? <LoginForm setStage={setstage}/> 
-                : stage === 'NewPassword' ? <NewPassword setStage={setstage}/>
-                : stage === 'Verification' ? <VerificationCode setStage={setstage}/>
-                : stage === 'Send' ? <SendCode setStage={setstage}/>
-                : ""
-            } </div>
+  return (
+    <div className="container-fluid fw-bold">
+      <div className="row">
+        <div className="col-xl-5 p-0 d-none d-lg-block vh-100">
+          <img
+            className="img-responsive"
+            style={{ objectFit: "cover" }}
+            src={login_image}
+          />
         </div>
-    );
+        {console.log(stage)}
+        {stage === "login" ? (
+          <LoginForm setStage={setstage} />
+        ) : stage === "NewPassword" ? (
+          <NewPassword setStage={setstage} />
+        ) : stage === "Verification" ? (
+          <VerificationCode setStage={setstage} />
+        ) : stage === "Send" ? (
+          <SendCode setStage={setstage} />
+        ) : (
+          ""
+        )}{" "}
+      </div>
+    </div>
+  );
 };
 export default Login;
